d2dcbb7f951d9e0e2d6cfa5bf3c1d23cee3c57e1
Giorgi Gigauri <wyalii@G.G>
2024-09-11 21:05:27 +0400

    configuration of project done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 function App() {
  return(
-  <div>
-    <Button>Hello World</Button>
-  </div>
+   <BrowserRouter>
+     <Routes>
+     <Route path='/auth' element={<Auth />} />
+     <Route path='/chat' element={<Chat />} />
+     <Route path='/profile' element={<Profile />} />
+     <Route path='*' element={<Navigate to = "/auth" />} />
+     </Routes>
+   </BrowserRouter>
  )
 }
 
