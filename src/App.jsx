ea5bb6843401d992547fe6a8855e88c337ec46fb
Giorgi Gigauri <wyalii@G.G>
2024-09-11 21:04:50 +0400

    configuration, tailwind, vite, shadcn,routes.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ return(
+  <div>
+    <Button>Hello World</Button>
+  </div>
+ )
+}
+
+export default App
