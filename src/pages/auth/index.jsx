3387bab7a236d4e28f8785aacf08a8ab47849a7e
Giorgi Gigauri <wyalii@G.G>
2024-09-12 21:07:26 +0400

    working on auth page.

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,6 +1,16 @@
 const Auth = () =>{
     return(
-        <div>Auth page.</div>
+        <div className=" h-[100vh] w-[100vw] flex items-center justify-center">
+            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90] lg:w-[70] xl:w-[60wv] rounded-3xl grid xl:grid-cols-2">
+                <div className="flex flex-col gap-10 items-center justify-center">
+                    <div className="flex items-center justify-center flex-col">
+                        <div className="flex items-center justify-center">
+                            <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
