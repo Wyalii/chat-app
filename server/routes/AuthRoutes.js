b7ccd3c257713711f5428a289f37cf81a00054d4
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2024-09-26 21:08:16 +0400

    added upload image, added routes, controllers for profile page.

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -4,11 +4,16 @@ import { login } from '../controllers/AuthController.js';
 import { getUserInfo } from '../controllers/AuthController.js';
 import { verifyToken } from '../middlewares/AuthMiddleware.js';
 import { updateProfile } from '../controllers/AuthController.js';
+import { addProfileImage } from '../controllers/AuthController.js';
+import { removeProfileImage } from '../controllers/AuthController.js';
+import multer from "multer"
 const authRoutes = Router();
-
+const upload = multer({dest:"uploads/profiles/"})
 authRoutes.post("/signup",signup)
 authRoutes.post("/login",login)
 authRoutes.get("/user-info",verifyToken,getUserInfo)
 authRoutes.post("/update-profile",verifyToken,updateProfile)
+authRoutes.post("/add-profile-image",verifyToken, upload.single("profile-image"), addProfileImage)
+authRoutes.post("/remove-profile-image",verifyToken,removeProfileImage)
 
 export default authRoutes
