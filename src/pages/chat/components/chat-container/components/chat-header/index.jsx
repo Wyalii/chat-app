f03de24aaa50363cfc580c3e78bd77361e1d18de
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2024-10-01 20:03:51 +0400

    chat header, message container, message bar. working...

---
+++ src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ChatHeader = () =>{
+    return(
+        <div>ChatHeader</div>
+    )
+}
+
+
+export default ChatHeader
---
+++ src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MessageBar = () =>{
+    return(
+        <div>MessageBar</div>
+    )
+}
+
+
+export default MessageBar
