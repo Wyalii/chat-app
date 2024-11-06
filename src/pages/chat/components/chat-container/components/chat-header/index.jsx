477c90acdfb0fd13df9478fc59ada73fc8743590
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2024-11-06 15:33:22 +0400

    added channels message area working on sending messages on channel chat

--- src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -14,17 +14,23 @@ const ChatHeader = () =>{
                 <div className="flex gap-3 items-center justify-center ">
 
                 <div className="w-12 h-12 relative">
-                    <Avatar className = "h-12 w-12 rounded-full overflow-hidden">
+                    {
+                        selectedChatType === "contact" ? <Avatar className = "h-12 w-12 rounded-full overflow-hidden">
                         {
                            selectedChatData.image ? (<AvatarImage src = {`${HOST}/${selectedChatData.image}`} alt = "Profile" className = "object-cover w-full h-full bg-black"></AvatarImage> ) :( 
                            <div className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(selectedChatData.color)}`}>
                              {selectedChatData.firstName ? selectedChatData.firstName.split("").shift() :selectedChatData.email.split("").shift()}
                            </div>
                         )}
-                    </Avatar>
+                    </Avatar> : <div className="bg-[#ffffff22] h-[10] w-[10] flex items-center justify-center rounded-full ">#</div>
+
+                    }
+                
+                    
                 </div>
 
                 <div>
+                    {selectedChatType === "contact" && selectedChatData.name}
                     {selectedChatType === "contact" && selectedChatData.firstName ?`${selectedChatData.firstName} ${selectedChatData.lastName}`: selectedChatData.email}
                 </div>
 
